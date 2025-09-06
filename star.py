cbc86bab3150f7157cb20c1f2da5fb80b4d8f30c
JoshuaBidjou123 <kristen.bidjou@gmail.com>
2025-09-06 12:44:50 -0400

    Commit

---
+++ star.py
@@ -0,0 +1,42 @@
+import turtle
+
+# creating canvas
+turtle.Screen(). bgcolor("Blue")
+
+sc = turtle.Screen()
+sc.setup(400, 300)
+
+turtle.title("Welcome to Turtle Window")
+
+# turtle object creation
+board = turtle.Turtle()
+
+# first triangle for star
+board.forward(100) # draw base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+board.penup()
+board.right(150)
+board.forward(50)
+
+# second triangle for star
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done
+
+
+    
+
